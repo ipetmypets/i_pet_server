c0039c5392331d33cb0b74c5a96033297d021576
BappiDeb <bappy08deb@gmail.com>
2025-01-06 13:40:19 +0600

    new

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,18 +3,19 @@ const User = require('../models/User');  // Assuming you have a User model
 // Get user profile
 const getUserProfile = async (req, res) => {
   try {
-    // The user id is attached to the request after authentication via JWT
     const userId = req.user.id;
-
-    // Find the user from the database
     const user = await User.findById(userId).select('-password');  // Exclude password from profile
 
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
 
-    // Respond with the user profile
-    res.json(user);
+    // Respond with the user profile data (username, email, location, etc.)
+    res.json({
+      username: user.username,
+      email: user.email,
+      location: user.Location,  // Include location data
+    });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Server error' });
