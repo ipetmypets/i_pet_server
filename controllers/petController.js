9c9ca2f0d85308b50818145aa3bb9956192742cb
BappiDeb <bappy08deb@gmail.com>
2025-01-07 20:36:37 +0600

    dad

--- controllers/petController.js
+++ controllers/petController.js
@@ -3,11 +3,16 @@ const PetProfile = require('../models/PetProfile');
 exports.createPetProfile = async (req, res) => {
   const { petName, petType, petPictures, petAge, petBreed, petDescription } = req.body;
 
+  let petPictureUrl = petPictures;
+  if (!petPictureUrl || petPictureUrl === '') {
+    petPictureUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgDFp3dmPTGj1xOEuSlAt-ilTfBOmYfth5hQ&s'; 
+  }
+
   const newPetProfile = new PetProfile({
     user: req.user.id, 
     petName,
     petType,
-    petPictures,
+    petPictures: petPictureUrl,  // Ensure the final petPictures is used
     petAge,
     petBreed,
     petDescription,
