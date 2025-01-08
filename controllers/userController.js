b62a93e0b129d6afa687cd20934bffcfe7fb59ac
BappiDeb <bappy08deb@gmail.com>
2025-01-08 14:15:47 +0600

    final

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,64 +27,35 @@ const getUserProfile = async (req, res) => {
   }
 };
 
-const deleteOldImage = async (imageUrl) => {
-  const imageId = imageUrl.split('/').pop().split('.')[0]; // Extract image ID from URL
-  const headers = {
-    'Authorization': `Client-ID ${IMGBUR_CLIENT_ID}`,
-  };
-
-  try {
-    const response = await axios.delete(`https://api.imgur.com/3/image/${imageId}`, { headers });
-    if (response.data.success) {
-      console.log('Old image deleted successfully');
-      return true;
-    }
-  } catch (err) {
-    console.error('Failed to delete old image:', err);
-    return false;
-  }
-};
-
-// Upload new image and update profile
 const uploadUserImage = async (req, res) => {
   try {
     if (!req.file) {
       return res.status(400).json({ error: 'No file uploaded' });
+      
+    }
+    const albumId = req.body.albumId;
+    if (!albumId) {
+      return res.status(400).json({ error: 'Album ID is required' });
     }
 
     const form = new FormData();
     const imagePath = req.file.path;
-    form.append('image', fs.createReadStream(imagePath)); // Add the image to the form
+    form.append('image', fs.createReadStream(imagePath));
+    form.append('album', albumId);
 
-    // Set the authorization headers with the Imgur client ID
     const headers = {
       'Authorization': `Client-ID ${IMGBUR_CLIENT_ID}`,
       ...form.getHeaders(),
     };
 
-    // Upload image to Imgur API
     const response = await axios.post('https://api.imgur.com/3/image', form, { headers });
-
-    fs.unlinkSync(req.file.path); // Remove the uploaded file from the server after uploading
+    fs.unlinkSync(req.file.path);
 
     if (response.data && response.data.success) {
-      const newImageUrl = response.data.data.link; // Get the uploaded image URL from the response
-
-      // Get current user
-      const user = await User.findById(req.user.id);
-
-      if (user.profile_pic) {
-        // If user already has a profile picture, delete the old one from Imgur
-        const oldImageDeleted = await deleteOldImage(user.profile_pic);
-        if (!oldImageDeleted) {
-          return res.status(500).json({ error: 'Failed to delete old image' });
-        }
-      }
-
-      // Update the user's profile with the new image URL
+      const imageUrl = response.data.data.link; 
       const updatedUser = await User.findByIdAndUpdate(
         req.user.id, 
-        { profile_pic: newImageUrl },  // Store the image URL in the user model
+        { profile_pic: imageUrl }, 
         { new: true }
       );
 
