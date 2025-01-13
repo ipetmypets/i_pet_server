81e382a43e42418b5541f851e9b69d98892b41ce
BappiDeb <bappy08deb@gmail.com>
2025-01-14 00:31:30 +0600

    new

--- routes/pet.js
+++ routes/pet.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createPetProfile, getPetProfiles, uploadPetPicture } = require('../controllers/petController');
+const { createPetProfile, getPetProfiles, uploadPetPicture, deletePetProfile } = require('../controllers/petController');
 const { checkAuth } = require('../middleware/authMiddleware');
 const multer = require('multer');
 
@@ -10,5 +10,6 @@ const upload = multer({ dest: 'uploads/' });
 router.post('/create', checkAuth, createPetProfile);
 router.get('/profiles', checkAuth, getPetProfiles);
 router.post('/uploadPetPicture', checkAuth, upload.single('petPicture'), uploadPetPicture); // Add this line
+router.delete('/profile/:profileId', checkAuth, deletePetProfile); // Add this line
 
 module.exports = router;
