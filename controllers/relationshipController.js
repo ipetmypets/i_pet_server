e7791f345dd545db79f079a873957b5bdaee7973
BappiDeb <bappy08deb@gmail.com>
2025-01-16 03:36:20 +0600

    check

--- controllers/relationshipController.js
+++ controllers/relationshipController.js
@@ -45,6 +45,7 @@ exports.sendFriendRequest = async (req, res) => {
 exports.updateFriendRequestStatus = async (req, res) => {
   const { sender_id, status } = req.body;
   const receiver_id = req.user.id;
+  console.log(sender_id, status, receiver_id);
 
   if (!['accepted', 'rejected'].includes(status)) {
     return res.status(400).json({ message: 'Invalid status. Must be either "accepted" or "rejected"' });
