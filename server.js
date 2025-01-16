0e03bdd96a1798d5fabb5c5f4467981cf16feaa0
BappiDeb <bappy08deb@gmail.com>
2025-01-16 19:11:44 +0600

    nice

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');
